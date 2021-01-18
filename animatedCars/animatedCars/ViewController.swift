fe8a24f6100c3160ee767a310fcbeee3585cc0ba
TonyJem <a.jemanov@gmail.com>
2021-01-19 00:45:55 +0200

    Add animation for all cars

--- animatedCars/animatedCars/ViewController.swift
+++ animatedCars/animatedCars/ViewController.swift
@@ -14,4 +14,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    @IBAction func runButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 3, delay: 0.5, options: .curveEaseIn) {
+            self.orangeCarConstraint.constant = self.view.bounds.maxY - self.orangeCar.frame.size.height * 1.7
+            self.view.layoutIfNeeded()
+        }
+        
+        UIView.animate(withDuration: 3, delay: 0.5, options: .curveLinear) {
+            self.greenCarConstraint.constant = self.view.bounds.maxY - self.greenCar.frame.size.height * 1.7
+            self.view.layoutIfNeeded()
+        }
+        
+        UIView.animate(withDuration: 3, delay: 0.5, options: .curveEaseOut) {
+            self.purpleCarConstraint.constant = self.view.bounds.maxY - self.purpleCar.frame.size.height * 1.7
+            self.view.layoutIfNeeded()
+        }
+    }
+    
 }
