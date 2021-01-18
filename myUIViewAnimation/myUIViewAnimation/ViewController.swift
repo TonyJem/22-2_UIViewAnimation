4b736c23586fe421b1d813aac105c662ba2a45b0
TonyJem <a.jemanov@gmail.com>
2021-01-18 23:32:04 +0200

    Add "moveSomeViewUpAndDownAnimated()"functionality

--- myUIViewAnimation/myUIViewAnimation/ViewController.swift
+++ myUIViewAnimation/myUIViewAnimation/ViewController.swift
@@ -9,12 +9,27 @@ class ViewController: UIViewController {
     }
     
     @IBAction func animateButtonTapped(_ sender: UIButton) {
-
+        moveSomeViewUpAndDownAnimated()
     }
     
     func changeBackgroundColorAnimated(to newColor: UIColor) {
         UIView.animate(withDuration: 1) {
             self.someView.backgroundColor = .blue
         }
+    }
+    
+    func moveSomeViewUpAndDownAnimated() {
+        
+        UIView.animateKeyframes(withDuration: 1, delay: 0, options: .calculationModeCubic) {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5) {
+                self.someView.center = CGPoint(x: self.view.bounds.midX, y: self.view.bounds.minY + self.someView.frame.size.height / 2)
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
+                self.someView.center = CGPoint(x: self.view.bounds.midX, y: self.view.bounds.maxY - self.someView.frame.size.height / 2)
+            }
+        }
+        
     }
 }
