451a6a21916491b63e66bf117a9c71a616173b5d
TonyJem <a.jemanov@gmail.com>
2021-01-18 23:13:47 +0200

    Create UI with Outlets and Action

--- myUIViewAnimation/myUIViewAnimation/ViewController.swift
+++ myUIViewAnimation/myUIViewAnimation/ViewController.swift
@@ -2,8 +2,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var someView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
     }
 
+    
+    @IBAction func animateButtonTapped(_ sender: UIButton) {
+        
+    }
 }
