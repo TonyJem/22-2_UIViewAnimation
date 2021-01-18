4e61d327373bc8cad65478f601c3b9cc0d06e554
TonyJem <a.jemanov@gmail.com>
2021-01-19 00:06:15 +0200

    Create UI with outlets and constraints

--- animatedCars/animatedCars/ViewController.swift
+++ animatedCars/animatedCars/ViewController.swift
@@ -2,8 +2,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var orangeCar: UIView!
+    @IBOutlet weak var greenCar: UIView!
+    @IBOutlet weak var purpleCar: UIView!
+    
+    @IBOutlet weak var orangeCarConstraint: NSLayoutConstraint!
+    @IBOutlet weak var greenCarConstraint: NSLayoutConstraint!
+    @IBOutlet weak var purpleCarConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
 }
